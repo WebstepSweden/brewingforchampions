21de2f42e6c2f795ef80b5a3f279dd5fd849e258
Johannes Lundsten <johannes.lundsten@gmail.com>
2017-04-08 19:00:36 +0200

    Created grid

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class PathFinder : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Grid grid = new Grid();
+
+    public class Grid
+    {
+
+        public float maxX = 4.5f;
+        public float minX = -4.5f;
+
+        public float maxZ = 9.5f;
+        public float minZ = -9.5f;
+
+        public float barrelXLength = 1f;
+        public float barrelZLength = 1f;
+
+        public Grid()
+        {
+            List<List<Tile>> tiles = new List<List<Tile>>();
+            var zIndexMax = (maxZ - minZ) / barrelZLength;
+            var xIndexMax = (maxX - minX) / barrelXLength;
+            for (var xIndex = 0; xIndex < xIndexMax + 1; xIndex++)
+            {
+                var xPoint = minX + xIndex * barrelXLength;
+                var currentList = new List<Tile>();
+                tiles.Add(currentList);
+
+                for (var zIndex = 0; zIndex < xIndexMax + 1; zIndex++)
+                {
+                    var zPoint = minZ + zIndex * barrelZLength;
+                    var tile = new Tile()
+                    {
+                        midPoint = new Vector3(xPoint, 1, zPoint),
+                        xIndex = xIndex,
+                        zIndex = zIndex,
+                        occupied = false
+                    };
+                    currentList.Add(tile);
+                }
+            }
+        }
+    }
+
+    public class Tile
+    {
+        public Vector3 midPoint;
+        public bool occupied;
+        public int xIndex;
+        public int zIndex;
+    }
+
 }
