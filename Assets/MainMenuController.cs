bd7e2684da4a9a11d993be41cb2d4c33301222e2
clanaa <claes.naatsaari@bisnode.com>
2017-04-14 14:52:41 +0200

    Updated main menu scene

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -1,16 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets;
+using System.Collections;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
-	// Use this for initialization
+	private DisruptiveApi.ApiClient api = new DisruptiveApi.ApiClient();
+
+	public Dropdown roomFarLeft;
+	public Dropdown roomFarRight;
+	public Dropdown roomNearLeft;
+	public Dropdown roomNearRight;
+
+	public Dropdown containerOne;
+	public Dropdown containerTwo;
+
+	public Button startButton;
+
 	void Start () {
+		StartCoroutine(api.GetSensors(GetSensors));
+	}
+
+	private void GetSensors(List<Sensor> sensors)
+	{
+		roomFarLeft.options.Clear();
+		foreach (Sensor sensor in sensors) {
+			string sensorLabel = (sensor.id + " (" + sensor.temperature + "°c) '" + sensor.name + "'");
+
+			// Populate room sensor dropdowns
+			roomFarLeft.options.Add (new Dropdown.OptionData(){ text=sensorLabel });
+			roomFarRight.options.Add (new Dropdown.OptionData(){ text=sensorLabel });
+			roomNearLeft.options.Add (new Dropdown.OptionData(){ text=sensorLabel });
+			roomNearRight.options.Add (new Dropdown.OptionData(){ text=sensorLabel });
+
+			// Populate container sensor dropdowns
+			containerOne.options.Add (new Dropdown.OptionData(){ text=sensorLabel });
+			containerTwo.options.Add (new Dropdown.OptionData(){ text=sensorLabel });
+		}
+
+
+		print (sensors.Count);
+	}
+
+	private void StartButton() {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	// Start Button:
+	// Read selected room sensor id's from dropdowns (x4)
+	// Read selected container sensor id's from dropdowns (x2)
+	// Read target temperature from textboxes (x2)
+	// Save Sensor id's to static map
+	// Save target temperatures to map
 }
