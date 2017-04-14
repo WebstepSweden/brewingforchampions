70a5720088f1b64a346a5b892884fdd86bb91e50
Johannes Lundsten <johannes.lundsten@gmail.com>
2017-04-14 13:33:02 +0200

    Added call to get all sensors

--- Assets/DisruptiveApi.cs
+++ Assets/DisruptiveApi.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,72 +13,48 @@ public class DisruptiveApi : MonoBehaviour {
 
         public IEnumerator GetSensors(string id, System.Action<float> resultCallback)
         {
-            using (var webClient = new UnityWebRequest(string.Format("https://api.disruptive-technologies.com/v1/things/" + id))) {
+            using (var webClient = new UnityWebRequest(string.Format("https://api.disruptive-technologies.com/v1/things/" + id)))
+            {
                 webClient.downloadHandler = new DownloadHandlerBuffer();
 
                 webClient.SetRequestHeader("Accept", "application/json");
                 webClient.SetRequestHeader("Authorization", "ApiKey d45a7c14c88f48f5937a8fc3254378ad");
                 webClient.SetRequestHeader("Content-Type", "application/json");
                 yield return webClient.Send();
-                
+
                 if (webClient.responseCode != 200) print("ERROR");
                 var jsonText = webClient.downloadHandler.text;
                 var subr = jsonText.Substring(jsonText.IndexOf("\"temperature") + "\"temperature:".Length + 1);
-                subr = subr.Substring(0, subr.IndexOf(","));               
+                subr = subr.Substring(0, subr.IndexOf(","));
                 resultCallback(float.Parse(subr));
             }
         }
-/**
-        public IEnumerator Put<T>(string path, string json, System.Action<ApiResponse<T>> resultCallback, System.Action<ApiResponse> errorCallback = null, string authKey = null)
+
+        public IEnumerator GetSensors(System.Action<List<Sensor>> resultCallback)
         {
-
-            using (var webClient = new UnityWebRequest(string.Format("http://{0}{1}", Host, path)))
+            using (var webClient = new UnityWebRequest(string.Format("https://api.disruptive-technologies.com/v1/things")))
             {
-
-                webClient.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(json.ToString()));
                 webClient.downloadHandler = new DownloadHandlerBuffer();
-                webClient.method = UnityWebRequest.kHttpVerbPUT;
-
-                if (!string.IsNullOrEmpty(authKey))
-                    webClient.SetRequestHeader("authenticationkey", authKey);
 
                 webClient.SetRequestHeader("Accept", "application/json");
+                webClient.SetRequestHeader("Authorization", "ApiKey d45a7c14c88f48f5937a8fc3254378ad");
                 webClient.SetRequestHeader("Content-Type", "application/json");
-
                 yield return webClient.Send();
 
+                if (webClient.responseCode != 200) print("ERROR");
                 var jsonText = webClient.downloadHandler.text;
-                if (Debug.isDebugBuild)
+                var sensorResponse = JsonUtility.FromJson<SensorsResponse>(jsonText);
+                print(JsonUtility.ToJson(sensorResponse));
+                List<Sensor> sensors = new List<Sensor>();
+                foreach(Sensor sensor in sensorResponse.things)
                 {
-                    Debug.Log("PUT result: " + jsonText);
-                    Debug.Log("PUT responseCode: " + webClient.responseCode);
-                }
-
-                if (webClient.responseCode != 200)
-                {
-                    if (errorCallback != null)
+                    if (sensor.hasTemperature)
                     {
-                        var response = new ApiResponse();
-                        response.ResponseCode = (int)webClient.responseCode;
-                        response.ResponseMessage = jsonText;
-                        Debug.LogError(path + "\n" + JsonUtility.ToJson(response));
-                        errorCallback(response);
+                        sensors.Add(sensor);
                     }
                 }
-                else
-                {
-                    if (resultCallback != null)
-                    {
-                        var response = new ApiResponse<T>();
-                        response.ResponseCode = (int)webClient.responseCode;
-                        if (!string.IsNullOrEmpty(jsonText))
-                        {
-                            response.Data = JsonUtility.FromJson<T>(jsonText);
-                        }
-                        resultCallback(response);
-                    }
-                }
+                resultCallback(sensors);
             }
-        }**/
+        }
     }
 }
